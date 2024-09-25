03ab2caa8af48029918a5fee80288efab408e772
Rommel Lee Briceño <khrbpersonal@gmail.com>
2024-09-25 01:37:09 -0400

    Add fetching of data

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,47 +7,6 @@ const mainFunc = () => {
 	`;
 	console.log("%c El script funciona", estilos);
 
-	// Object with Icons and Titles
-	// const data = {
-	// 	type: [
-	// 		"fa-solid",
-	// 		"fa-regular",
-	// 		"fa-regular",
-	// 		"fa-solid",
-	// 		"fa-solid",
-	// 		"fa-solid"
-	// 	],
-	// 	icons: [
-	// 		"fa-home",
-	// 		"fa-calendar",
-	// 		"fa-bookmark",
-	// 		"fa-chalkboard-user",
-	// 		"fa-music",
-	// 		"fa-list"
-	// 	],
-	// 	titles: [
-	// 		"Home",
-	// 		"Calendar",
-	// 		"Links",
-	// 		"Tutorials",
-	// 		"Music",
-	// 		{
-	// 			title: "Apps",
-	// 			components: [
-	// 				"HTML",
-	// 				"CSS",
-	// 				"Javascript"
-	// 			]
-	// 		}
-	// 	]
-	// }
-
-	const data = {
-		type: ["fa-regular"],
-		icons: ["fa-circle"],
-		titles: ["Home"]
-	}
-
 	// Assign html elements to variables (left panel and main content)
 	const panel = document.querySelector("#panel"),
 		  viewboard = document.querySelector("#viewboard");
@@ -56,84 +15,100 @@ const mainFunc = () => {
 	panel.classList.add("panel-Default");
 	viewboard.classList.add("viewboard-Default");
 
-	// Loading items
-	for (let i = 0; i < data.titles.length; i++) {
+	// Fetch data
+	fetch("assets/js/userApps.json")
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw new Error("Error en la respuesta: " + response.status);
+			}
+		})
+		.then(data => {
+			console.log("Data received: ", data);
+			load(data);
+		})
 
-		// DOM Nodes
-		const item = document.createElement("div"),				// div item
-			  wrapper = document.createElement("div"),			// wrapper for icon and title
-			  icon = document.createElement("i"),				// font awesome icon
-			  title = document.createElement("span");			// item title
+	// Load items
+	const load = data => {
+		for (let i = 0; i < data.titles.length; i++) {
 
-		// Append
-		item.appendChild(wrapper);
-		wrapper.appendChild(icon);
-		wrapper.appendChild(title)
-		panel.appendChild(item);
-
-		// Add classes
-		item.classList.add("panel_item");
-		wrapper.classList.add("item_wrapper");
-		icon.classList.add("item_icon", data.type[i], data.icons[i]);
-		title.classList.add("item_title");
-		
-
-		// Value if it's an object
-		if (typeof data.titles[i] == "object") {
-
-			title.innerText = data.titles[i].title;
-
-			// DOM nodes
-			const menu = document.createElement("div");			// dropdown menu
+			// DOM Nodes
+			const item = document.createElement("div"),				// div item
+				  wrapper = document.createElement("div"),			// wrapper for icon and title
+				  icon = document.createElement("i"),				// font awesome icon
+				  title = document.createElement("span");			// item title
 
 			// Append
-			item.appendChild(menu);
+			item.appendChild(wrapper);
+			wrapper.appendChild(icon);
+			wrapper.appendChild(title)
+			panel.appendChild(item);
 
 			// Add classes
-			item.classList.add("menu_dropdown");
-			wrapper.classList.add("item_collapsed");
-			menu.classList.add("item_menu", "no-display");
+			item.classList.add("panel_item");
+			wrapper.classList.add("item_wrapper");
+			icon.classList.add("item_icon", data.type[i], data.icons[i]);
+			title.classList.add("item_title");
 			
 
-			// Create menu elements
-			for (let j = 0; j < data.titles[i].components.length; j++) {
-				
-				// Declaration
-				const element = document.createElement("div");
+			// Value if it's an object
+			if (typeof data.titles[i] == "object") {
+
+				title.innerText = data.titles[i].title;
+
+				// DOM nodes
+				const menu = document.createElement("div");			// dropdown menu
 
 				// Append
-				menu.appendChild(element);
+				item.appendChild(menu);
 
 				// Add classes
-				element.classList.add("menu_option");
+				item.classList.add("menu_dropdown");
+				wrapper.classList.add("item_collapsed");
+				menu.classList.add("item_menu", "no-display");
+				
 
-				// Add text
-				element.innerText = data.titles[i].components[j];
+				// Create menu elements
+				for (let j = 0; j < data.titles[i].components.length; j++) {
+					
+					// Declaration
+					const element = document.createElement("div");
+
+					// Append
+					menu.appendChild(element);
+
+					// Add classes
+					element.classList.add("menu_option");
+
+					// Add text
+					element.innerText = data.titles[i].components[j];
+
+				}
+
+				// Add event listeners
+				wrapper.addEventListener(
+					"click", 
+					function (){
+						wrapper.classList.toggle("item_collapsed");
+						wrapper.classList.toggle("item_expanded");
+						menu.classList.toggle("no-display");
+					});
+
+			} else {
+
+				title.innerText = data.titles[i];
 
 			}
 
-			// Add event listeners
-			wrapper.addEventListener(
-				"click", 
-				function (){
-					wrapper.classList.toggle("item_collapsed");
-					wrapper.classList.toggle("item_expanded");
-					menu.classList.toggle("no-display");
-				});
-
-		} else {
-
-			title.innerText = data.titles[i];
-
-		}
-
-	} // End of for
-} // End of function
+		} // End of for
+	} // End of load function
+} // End of main function
 
 // Function load
 window.onload = mainFunc;
 
-/* Toggle for panel */
+/* Panel Toggle */
 
 const panelBtnToggle = document.querySelector("#panelBtnToggle");
 const panelToggle = () => {
